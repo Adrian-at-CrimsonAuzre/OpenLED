14688e323fd89a9fef8abfe5bcf0b60d60b2a408
Adrian Garcia <agarcia@daytonparts.com>
2018-02-02 17:27:27 -0500

    Ported Arduino Client from POC project

--- Arduino/Arduino Client/Arduino Client.ino
+++ Arduino/Arduino Client/Arduino Client.ino
@@ -1,12 +1,26 @@
-﻿void setup() {
-	// initialize digital pin LED_BUILTIN as an output.
-	pinMode(LED_BUILTIN, OUTPUT);
+﻿#include <FastLED.h>
+#define NUM_LEDS 288
+#define DATA_PIN 2
+
+CRGB leds[NUM_LEDS];
+
+void setup()
+{
+	Serial.begin(2000000);
+	FastLED.addLeds<WS2812B, DATA_PIN, GRB>(leds, NUM_LEDS);
 }
 
-// the loop function runs over and over again forever
-void loop() {
-	digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-	delay(1000);                       // wait for a second
-	digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-	delay(1000);                       // wait for a second
+void loop()
+{
+	byte SerialRGBIn[3];
+
+	//wait for serial comms
+	while (!Serial.available()) {}
+
+	//read in 3 bytes of color information
+	for (int i = 0; i < 3; i++)
+		SerialRGBIn[i] = Serial.read();
+
+	//brightness correction, since these LEDs are bright as hell
+	FastLED.showColor(CRGB(((int)SerialRGBIn[0]) / 4, ((int)SerialRGBIn[1]) / 4, ((int)SerialRGBIn[2]) / 4));
 }
