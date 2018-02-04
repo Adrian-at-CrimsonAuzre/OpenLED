0d58a829a07d899cbbfe351bea011a0589d6b527
Adrian Garcia <agarcia@daytonparts.com>
2018-02-04 18:04:45 -0500

    Better brightness curve, turns off the fucking LEDs when nothing is happening

--- Arduino/Arduino Client/Arduino Client.ino
+++ Arduino/Arduino Client/Arduino Client.ino
@@ -8,6 +8,9 @@ void setup()
 {
 	Serial.begin(115200);
 	FastLED.addLeds<WS2812B, DATA_PIN, GRB>(leds, NUM_LEDS);
+
+	//turn them off when we first start
+	FastLED.showColor(CRGB(0, 0, 0));
 }
 
 void loop()
@@ -15,11 +18,23 @@ void loop()
 	byte SerialRGBIn[3];
 
 	//wait for serial comms
-	while (!Serial.available()) {}
+	int resetLEDs = 0;
+	while (!Serial.available())
+	{
+		//Set LEDs to black if we haven't gotten sound data
+		resetLEDs++;
+		if (resetLEDs >= 16000000)
+		{
+			resetLEDs = 0;
+			FastLED.showColor(CRGB(0, 0, 0));
+		}
+	}
 
 	//read in 3 bytes of color information
 	Serial.readBytes(SerialRGBIn, 3);
+	for (int i = 0; i < 3; i++)
+		SerialRGBIn[i] = round(pow((double)SerialRGBIn[i], 1/1.25));
 
 	//brightness correction, since these LEDs are bright as hell
-	FastLED.showColor(CRGB(((int)SerialRGBIn[0]) / 4, ((int)SerialRGBIn[1]) / 4, ((int)SerialRGBIn[2]) / 4));
+	FastLED.showColor(CRGB((int)SerialRGBIn[0], (int)SerialRGBIn[1], (int)SerialRGBIn[2]));
 }
