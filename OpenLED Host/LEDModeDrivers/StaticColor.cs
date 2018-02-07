c6d89c18010b7008bfe00ee2c5ec99165da6be89
Adrian Garcia <agarcia@daytonparts.com>
2018-02-06 21:19:39 -0500

    Added logic to properly switch between modes (kinda)

---
+++ OpenLED Host/LEDModeDrivers/StaticColor.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace OpenLED_Host.LEDModeDrivers
+{
+    class StaticColor : LEDModeBase
+    {
+		private HSLColor _Color;
+		/// <summary>
+		/// The Color that is supposed to be set
+		/// </summary>
+		public HSLColor Color
+		{
+			get { return _Color; }
+			set
+			{
+				_Color = value;
+				ColorOut(Color);
+				NotifyPropertyChanged();
+			}
+		}
+
+	}
+}
--- OpenLED Host/ViewModels/MainWindowViewModel.cs
+++ OpenLED Host/ViewModels/MainWindowViewModel.cs
@@ -11,6 +11,36 @@ namespace OpenLED_Host.ViewModels
 {
 	class MainWindowViewModel : NotifyBase
 	{
+		private LEDModes _LEDMode = LEDModes.NULL;
+		/// <summary>
+		/// Current LED Mode of the application
+		/// </summary>
+		public LEDModes LEDMode
+		{
+			get { return _LEDMode; }
+			set
+			{
+				_LEDMode = value;
+
+				switch(LEDMode)
+				{
+					case (LEDModes.VolumeAndPitchReactive):
+						{
+							VolumeAndPitch.StartReacting();
+							break;
+						}
+					case (LEDModes.SingleColor):
+						{
+							VolumeAndPitch.StopReacting();
+							break;
+						}
+				}
+
+				NotifyPropertyChanged();
+			}
+		}
+
+
 		private VolumeAndPitchReactive _VolumeAndPitch = new VolumeAndPitchReactive();
 		/// <summary>
 		/// Backend for lighting
@@ -25,27 +55,70 @@ namespace OpenLED_Host.ViewModels
 			}
 		}
 
-
+		private StaticColor _StaticColor = new StaticColor();
 		/// <summary>
-		/// Visualizer being displayed to user, only reason it's here is to be able to access some of its methods
+		/// The Static Color property
 		/// </summary>
-		public Spectrum Visualizer;
+		public StaticColor StaticColor
+		{
+			get { return _StaticColor; }
+			set
+			{
+				_StaticColor = value;
+				NotifyPropertyChanged();
+			}
+		}
+
+		private HSLColor _ColorOne = new HSLColor(0, 0, 0);
+		/// <summary>
+		/// Color One of various effects
+		/// </summary>
+		public HSLColor ColorOne
+		{
+			get { return _ColorOne; }
+			set
+			{
+				_ColorOne = value;
+				if (LEDMode == LEDModes.SingleColor)
+					StaticColor.Color = ColorOne;
+				NotifyPropertyChanged();
+			}
+		}
+
+		private HSLColor _ColorTwo = new HSLColor(0, 0, 0);
+		/// <summary>
+		/// Color Two of various effects
+		/// </summary>
+		public HSLColor ColorTwo
+		{
+			get { return _ColorTwo; }
+			set
+			{
+				_ColorTwo = value;
+				NotifyPropertyChanged();
+			}
+		}
 
 
-		private int _TabControlSelected = 0;
+
+
+		private System.Windows.Controls.TabItem _TabControlSelected;
 		/// <summary>
 		/// Index of the MainWindo TabControl
 		/// </summary>
-		public int TabControlSelected
+		public System.Windows.Controls.TabItem TabControlSelected
 		{
 			get { return _TabControlSelected; }
 			set
 			{
-				//if current tab is 6 and new tab is not 6
-				if (_TabControlSelected == 6 && value != 6)
-					Visualizer.AnimatingState(false);
-				else if (_TabControlSelected != 6 && value == 6)
-					Visualizer.AnimatingState(true);
+				if(LEDMode == LEDModes.VolumeAndPitchReactive)
+					//If the current item is the visualizer, and we're switching away from it, turn it off
+					if ((string)value.Header != "Visualizer" && TabControlSelected != null && (string)TabControlSelected.Header == "Visualizer")
+						(TabControlSelected.Content as Spectrum).AnimatingState(false);
+					//If the new value is the visualizer, turn it on
+					else if((string)value.Header == "Visualizer")
+						(value.Content as Spectrum).AnimatingState(true);
+
 				_TabControlSelected = value;
 
 				NotifyPropertyChanged();
